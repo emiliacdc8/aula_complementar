0a6de1784d182a33bd99aa3b58be5c918120f2bc
emilia <emiliacdc8@gmail.com>
2025-06-07 00:01:28 -0300

    alunos_disciplinas_GET-POST-PUT-DELETE

--- src/main/java/com/example/demo/controller/AlunoController.java
+++ src/main/java/com/example/demo/controller/AlunoController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.model.Aluno;
 import com.example.demo.repository.AlunoRepository;
@@ -24,9 +31,34 @@ public class AlunoController {
 		return list;
 	}
 	
-	@GetMapping(value="/{id}")//para buscar uma disciplina pelo id
+	@GetMapping(value="/{id}")//para buscar um aluno pelo id
 	public Aluno listarUm(@PathVariable Integer id) {
 		Optional<Aluno> obj = repository.findById(id);
 		return obj.orElse(null);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> inserir(@RequestBody Aluno obj){
+		obj = repository.save(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentContextPath().path("{}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value="/{id}")//alteração baseada em um id
+	public ResponseEntity<Void> atualizar(@RequestBody Aluno obj, @PathVariable Integer id){
+		Optional<Aluno> objF = repository.findById(id);
+		objF.get().setNome(obj.getNome());
+		objF.get().setEmail(obj.getEmail());
+		objF.get().setMatricula(obj.getMatricula());
+		objF.get().setCurso(obj.getCurso());
+		obj = repository.save(objF.get());//grava objF
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{id}")//Deleta baseado em um id
+	public ResponseEntity<Void> excluir(@PathVariable Integer id){
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
